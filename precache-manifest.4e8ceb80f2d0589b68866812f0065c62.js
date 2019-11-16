709af58c43a6236dc0ebd72d1d2b721e722b73b2
Khoa Nguyen <meo.wizard@gmail.com>
2019-11-16 18:42:46 +0700

    Updates

---
+++ precache-manifest.4e8ceb80f2d0589b68866812f0065c62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4818ff59bf77d98fa3599918c691f3ab",
+    "url": "/login-page/index.html"
+  },
+  {
+    "revision": "ac3e774a5c703f24f90b",
+    "url": "/login-page/static/css/main.c304409f.chunk.css"
+  },
+  {
+    "revision": "40002ec6abf141a0f321",
+    "url": "/login-page/static/js/2.f0859a52.chunk.js"
+  },
+  {
+    "revision": "ac3e774a5c703f24f90b",
+    "url": "/login-page/static/js/main.c595107a.chunk.js"
+  },
+  {
+    "revision": "806d0df0349265eea64b",
+    "url": "/login-page/static/js/runtime-main.2a8e31f4.js"
+  }
+]);
